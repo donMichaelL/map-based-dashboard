96e4c7c8eea8e18eaed37e3dc85a0d2c559c0f74
Michael Loukeris <michael.loukeris@icloud.com>
2020-06-13 11:35:44 +0300

    Adding heroku config files

--- config/settings/local.py
+++ config/settings/local.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(INITIAL_BASE_DIR)
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
