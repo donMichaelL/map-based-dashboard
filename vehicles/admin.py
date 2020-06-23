4d2025d57284d3f3361a36a130163b368fe2f292
Michael Loukeris <michael.loukeris@icloud.com>
2020-06-23 12:22:46 +0300

    Create admin UI

--- vehicles/admin.py
+++ vehicles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Vehicle
 
-# Register your models here.
+
+@admin.register(Vehicle)
+class VehicleModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'description', 'status', 'isAvailable')
+
+
+
