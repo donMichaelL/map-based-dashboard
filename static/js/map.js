8ed27a1921eb5db77bc74d3f117a771b5f326a24
Michael Loukeris <michael.loukeris@icloud.com>
2020-06-24 12:28:24 +0300

    Adding zoom in menu item when right click

--- static/js/map.js
+++ static/js/map.js
@@ -1,69 +1,66 @@
+var currentView = new ol.View({
+  center: ol.proj.fromLonLat([20.4370, 38.2022]),
+  zoom: 6
+});
+
 var map = new ol.Map({
     target: 'map',
     layers: layers,
-    view: new ol.View({
-      center: ol.proj.fromLonLat([20.4370, 38.2022]),
-      zoom: 6
-    })
+    view: currentView
   });
 
   
-  map.on('click', function(evt) {
-    $("#mapMenu").removeClass("show").hide();
-      var feature = map.forEachFeatureAtPixel(evt.pixel,
-        function(feature) {
-          return feature;
-        });
-      if (uavFeatures.includes(feature) | uuvFeatures.includes(feature) |
-          ugvFeatures.includes(feature) | usvFeatures.includes(feature)) {
-        source.clear();
-        var start = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
-        var coordinates = [ start, [getRandomInRange(20, 26, 4), getRandomInRange(33, 38, 4)], [getRandomInRange(20, 26, 4), getRandomInRange(33, 38, 4)],[getRandomInRange(20, 26, 4), getRandomInRange(33, 38, 4)],[getRandomInRange(20, 26, 4), getRandomInRange(33, 38, 4)],];
-        var lineGeom = new ol.geom.LineString(coordinates);
-        lineGeom.transform('EPSG:4326', 'EPSG:3857');
-        var lineFeature = new ol.Feature(lineGeom);
-        source.addFeatures([lineFeature]);
-
-
-        flightsSource.clear();
-        var start = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
-        var start = { x: start[0], y: start[1] };
-        var end = { x: 28.979530, y: 41.015137 };
-        var arcGenerator = new arc.GreatCircle(start, end);
-        var arcLine = arcGenerator.Arc(100, {offset: 10});
-        if (arcLine.geometries.length === 1) {
-          var line = new ol.geom.LineString(arcLine.geometries[0].coords);
-          line.transform(ol.proj.get('EPSG:4326'), ol.proj.get('EPSG:3857'));
-          var feature = new ol.Feature({
-              geometry: line,
-              finished: false
-          });
-          flightsSource.addFeature(feature)
-        }
-      } else {
-        console.log("Stou koutrouli ton gamo");
-      }
+map.on('click', function(evt) {
+  $("#mapMenu").removeClass("show").hide();
+  var feature = map.forEachFeatureAtPixel(evt.pixel,
+    function(feature) {
+      return feature;
     });
-
-
-  // // Right Click
-    map.addEventListener('contextmenu', function(e) {
-      e.preventDefault();
-      var feature = map.forEachFeatureAtPixel(e.pixel,  function(feature) {
-        return feature;
-      });
-      if (uavFeatures.includes(feature) | uuvFeatures.includes(feature) |
+  if (uavFeatures.includes(feature) | uuvFeatures.includes(feature) |
       ugvFeatures.includes(feature) | usvFeatures.includes(feature)) {
+    source.clear();
+    var start = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
+    var coordinates = [ start, [getRandomInRange(20, 26, 4), getRandomInRange(33, 38, 4)], [getRandomInRange(20, 26, 4), getRandomInRange(33, 38, 4)],[getRandomInRange(20, 26, 4), getRandomInRange(33, 38, 4)],[getRandomInRange(20, 26, 4), getRandomInRange(33, 38, 4)],];
+    var lineGeom = new ol.geom.LineString(coordinates);
+    lineGeom.transform('EPSG:4326', 'EPSG:3857');
+    var lineFeature = new ol.Feature(lineGeom);
+    source.addFeatures([lineFeature]);
 
-      } else {
-        $("#mapMenu").css({
-          display: "block",
-          top: e.originalEvent.pageY - 6,
-          left: e.originalEvent.pageX
-        }).addClass("show");
-        console.log(e.originalEvent.pageX);
-      }
-    }, false);
+
+    flightsSource.clear();
+    var start = ol.proj.transform(evt.coordinate, 'EPSG:3857', 'EPSG:4326');
+    var start = { x: start[0], y: start[1] };
+    var end = { x: 28.979530, y: 41.015137 };
+    var arcGenerator = new arc.GreatCircle(start, end);
+    var arcLine = arcGenerator.Arc(100, {offset: 10});
+    if (arcLine.geometries.length === 1) {
+      var line = new ol.geom.LineString(arcLine.geometries[0].coords);
+      line.transform(ol.proj.get('EPSG:4326'), ol.proj.get('EPSG:3857'));
+      var feature = new ol.Feature({
+          geometry: line,
+          finished: false
+      });
+      flightsSource.addFeature(feature)
+    }
+  } else {
+    console.log("Stou koutrouli ton gamo");
+  }
+});
+
+ // Right Click
+ var rightClickPosition;
+map.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+  rightClickPosition = ol.proj.transform(e.coordinate, 'EPSG:3857', 'EPSG:4326');
+  var feature = map.forEachFeatureAtPixel(e.pixel,  function(feature) {
+    return feature;
+  });
+  $("#mapMenu").css({
+    display: "block",
+    top: e.originalEvent.pageY - 6,
+    left: e.originalEvent.pageX
+  }).addClass("show");
+}, false);
 
 // Geolocation User ///////////////////////////////////////////////////
     var view = new ol.View({
@@ -93,3 +90,17 @@ var map = new ol.Map({
 ///////////////////////////////////////////////////
 
 $(".dropdown-item").on('contextmenu', function(e) { e.preventDefault();});
+document.getElementById('zoomContextMenu').addEventListener('click', function(e){
+  animateTo(rightClickPosition);
+});
+
+
+function animateTo(location, duration=2000 ,zoom=7) {
+  $("#mapMenu").removeClass("show").hide();
+  currentView.animate({
+    center: ol.proj.fromLonLat(location),
+    duration: duration,
+    zoom: zoom
+  }, function(){ 
+  });
+}
